38720c0b35e19f2137d3b575f320bfd63c9ae8be
kingtut1 <pharoahcampbell@gmail.com>
2020-04-22 05:40:14 -0400

    Book Room page finished mostly and start of review reservation page.

--- NovaRegistration/BackRoomViewController.swift
+++ NovaRegistration/BackRoomViewController.swift
@@ -25,6 +25,73 @@ class BackRoomViewController: UIViewController {
     @IBOutlet weak var Room110Button: UIButton!
     @IBOutlet weak var RoomButton111Button: UIButton!
     
+    @IBAction func Button101Pressed(_ sender: Any) {
+        print("101Pressed", Room101Button.title(for: .normal))
+        if Room101Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+        
+    }
+    @IBAction func Button102Pressed(_ sender: Any) {
+        print("102Pressed", Room102Button.title(for: .normal))
+        if Room102Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button103Pressed(_ sender: Any) {
+        print("103Pressed", Room103Button.title(for: .normal))
+        if Room103Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button104Pressed(_ sender: Any) {
+        print("104Pressed", Room104Button.title(for: .normal))
+        if Room104Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button105Pressed(_ sender: Any) {
+        print("105Pressed", Room105Button.title(for: .normal))
+        if Room105Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button106Pressed(_ sender: Any) {
+        print("106Pressed", Room106Button.title(for: .normal))
+        if Room106Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button107Pressed(_ sender: Any) {
+        print("107Pressed", Room107Button.title(for: .normal))
+        if Room107Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button108Pressed(_ sender: Any) {
+        print("108Pressed", Room108Button.title(for: .normal))
+        if Room108Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button109Pressed(_ sender: Any) {
+        print("109Pressed", Room109Button.title(for: .normal))
+        if Room109Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button110Pressed(_ sender: Any) {
+        print("110Pressed", Room110Button.title(for: .normal))
+        if Room110Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
+    @IBAction func Button111Pressed(_ sender: Any) {
+        print("111Pressed", RoomButton111Button.title(for: .normal))
+        if RoomButton111Button.backgroundColor == UIColor.green{
+            print("Book Room")
+        }
+    }
     
     
     
@@ -52,8 +119,32 @@ class BackRoomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Room101Button.backgroundColor = UIColor.darkGray
+        Room101Button.frame = CGRect(x:113 ,y:266 , width:30 , height:30)
+        Room102Button.frame = CGRect(x:119 ,y:337 , width:30 , height:30)
+        Room103Button.frame = CGRect(x:119 ,y:381 , width:30 , height:30)
+        Room104Button.frame = CGRect(x:119 ,y:425 , width:30 , height:30)
+        Room105Button.frame = CGRect(x:119 ,y:463 , width:30 , height:30)
+        Room106Button.frame = CGRect(x:119 ,y:510 , width:30 , height:30)
+        Room107Button.frame = CGRect(x:119 ,y:631 , width:30 , height:30)
+        Room108Button.frame = CGRect(x:192 ,y:637 , width:30 , height:30)
+        Room109Button.frame = CGRect(x:192 ,y:510 , width:30 , height:30)
+        Room110Button.frame = CGRect(x:192 ,y:336 , width:30 , height:30)
+        RoomButton111Button.frame = CGRect(x:192 ,y:294 , width:30 , height:30)
         Room101Button.layer.cornerRadius = 15
+        Room102Button.layer.cornerRadius = 15
+        Room103Button.layer.cornerRadius = 15
+        Room104Button.layer.cornerRadius = 15
+        Room105Button.layer.cornerRadius = 15
+        Room106Button.layer.cornerRadius = 15
+        Room107Button.layer.cornerRadius = 15
+        Room108Button.layer.cornerRadius = 15
+        Room109Button.layer.cornerRadius = 15
+        Room110Button.layer.cornerRadius = 15
+        RoomButton111Button.layer.cornerRadius = 15
+        BookRoomImage.image = RoomImages[0]
+        
+        
+        
         
         self.RoomSelector.delegate = self
         self.RoomSelector.dataSource = self
@@ -74,18 +165,6 @@ class BackRoomViewController: UIViewController {
         FloorData = [1,2,3]
         RoomSelector.selectRow(0, inComponent: 0, animated: true)
         RoomSelector.selectRow(0, inComponent: 0, animated: true)
-        
-        Room101Button.frame = CGRect(x:71 ,y:158 , width:30 , height:30)
-        Room102Button.frame = CGRect(x:71 ,y:247 , width:30 , height:30)
-        Room103Button.frame = CGRect(x:71 ,y:322 , width:30 , height:30)
-        Room104Button.frame = CGRect(x:71 ,y:406 , width:30 , height:30)
-        Room105Button.frame = CGRect(x:117 ,y:412 , width:30 , height:30)
-        Room106Button.frame = CGRect(x:192 ,y:631 , width:30 , height:30)
-        Room107Button.frame = CGRect(x:236 ,y:631 , width:30 , height:30)
-        Room108Button.frame = CGRect(x:274 ,y:631 , width:30 , height:30)
-        Room109Button.frame = CGRect(x:319 ,y:631 , width:30 , height:30)
-        Room110Button.isHidden = true
-        RoomButton111Button.isHidden = true
         
         // Do any additional setup after loading the view.
     }
@@ -125,18 +204,9 @@ extension BackRoomViewController:UIPickerViewDelegate, UIPickerViewDataSource{
             print("Title:", RoomData[row])
             if RoomData[row] == "Leo Goodwin Sr. Residence Hall"
             {
+                print("Entered goodwin")
                 BookRoomImage.image = RoomImages[row]
-                Room101Button.setTitle("101", for: .normal)
-                Room102Button.setTitle("102", for: .normal)
-                Room103Button.setTitle("103", for: .normal)
-                Room104Button.setTitle("104", for: .normal)
-                Room105Button.setTitle("105", for: .normal)
-                Room106Button.setTitle("106", for: .normal)
-                Room107Button.setTitle("107", for: .normal)
-                Room108Button.setTitle("108", for: .normal)
-                Room109Button.setTitle("109", for: .normal)
-                Room110Button.setTitle("110", for: .normal)
-                RoomButton111Button.setTitle("111", for: .normal)
+                
                 
                 Room101Button.frame = CGRect(x:113 ,y:266 , width:30 , height:30)
                 Room102Button.frame = CGRect(x:119 ,y:337 , width:30 , height:30)
@@ -148,9 +218,21 @@ extension BackRoomViewController:UIPickerViewDelegate, UIPickerViewDataSource{
                 Room108Button.frame = CGRect(x:192 ,y:637 , width:30 , height:30)
                 Room109Button.frame = CGRect(x:192 ,y:510 , width:30 , height:30)
                 Room110Button.frame = CGRect(x:192 ,y:336 , width:30 , height:30)
-                //RoomButton111Button.frame = CGRect(x:192 ,y:294 , width:30 , height:30)
-                RoomButton111Button.isHidden = true
+                RoomButton111Button.frame = CGRect(x:192 ,y:294 , width:30 , height:30)
+                RoomButton111Button.isHidden = false
                 Room104Button.isHidden = false
+                
+                Room101Button.setTitle("101", for: .normal)
+                Room102Button.setTitle("102", for: .normal)
+                Room103Button.setTitle("103", for: .normal)
+                Room104Button.setTitle("104", for: .normal)
+                Room105Button.setTitle("105", for: .normal)
+                Room106Button.setTitle("106", for: .normal)
+                Room107Button.setTitle("107", for: .normal)
+                Room108Button.setTitle("108", for: .normal)
+                Room109Button.setTitle("109", for: .normal)
+                Room110Button.setTitle("110", for: .normal)
+                RoomButton111Button.setTitle("111", for: .normal)
             }
             else if RoomData[row] == "The Commons"
             {
@@ -184,13 +266,13 @@ extension BackRoomViewController:UIPickerViewDelegate, UIPickerViewDataSource{
                 Room101Button.setTitle("101", for: .normal)
                 Room102Button.setTitle("102", for: .normal)
                 Room103Button.setTitle("103", for: .normal)
-                Room104Button.setTitle("104", for: .normal)
+                Room104Button.setTitle("Test2", for: .normal)
                 Room105Button.setTitle("105", for: .normal)
                 Room106Button.setTitle("106", for: .normal)
                 Room107Button.setTitle("107", for: .normal)
                 Room108Button.setTitle("108", for: .normal)
                 Room109Button.setTitle("109", for: .normal)
-                Room110Button.setTitle("110", for: .normal)
+                Room110Button.setTitle("Test1", for: .normal)
                 RoomButton111Button.setTitle("111", for: .normal)
                 Room101Button.frame = CGRect(x:150 ,y:191 , width:30 , height:30)
                 Room102Button.frame = CGRect(x:150 ,y:247 , width:30 , height:30)
@@ -357,11 +439,25 @@ extension BackRoomViewController:UIPickerViewDelegate, UIPickerViewDataSource{
         }
         if RoomSelected == true && FloorSelected == true
         {
+            DispatchQueue.main.async {
+                self.Room101Button.backgroundColor = UIColor.gray
+                self.Room102Button.backgroundColor = UIColor.gray
+                self.Room103Button.backgroundColor = UIColor.gray
+                self.Room104Button.backgroundColor = UIColor.gray
+                self.Room105Button.backgroundColor = UIColor.gray
+                self.Room106Button.backgroundColor = UIColor.gray
+                self.Room107Button.backgroundColor = UIColor.gray
+                self.Room108Button.backgroundColor = UIColor.gray
+                self.Room109Button.backgroundColor = UIColor.gray
+                self.Room110Button.backgroundColor = UIColor.gray
+                self.RoomButton111Button.backgroundColor = UIColor.gray
+                
+            }
             print("Can call api now")
             print("key: ", RoomKey[keyIndex])
             //Call API
-            //var APIString = "http://nova.us-east-2.elasticbeanstalk.com/api/Project?BuildingName=" + RoomKey[keyIndex]+"&FloorNumber="+String(FloorData[row])
-            var APIString = "http://nova.us-east-2.elasticbeanstalk.com/api/GetAvailableRoomsByFloor?BuildingName=COM&FloorNumber=2"
+            var APIString = "http://nova.us-east-2.elasticbeanstalk.com/api/GetAvailableRoomsByFloor?BuildingName=" + RoomKey[keyIndex]+"&FloorNumber="+String(FloorData[row])
+            //var APIString = "http://nova.us-east-2.elasticbeanstalk.com/api/GetAvailableRoomsByFloor?BuildingName=COM&FloorNumber=2"
             print("API call: ", APIString)
             let requestURL = URL(string: String(APIString))//1")
             var request = URLRequest(url:requestURL!)
@@ -385,24 +481,79 @@ extension BackRoomViewController:UIPickerViewDelegate, UIPickerViewDataSource{
                     print("Start")
                     let rooms = try
                         JSONDecoder().decode([Room].self, from: data!)
-                    /*
                     print(rooms)
-                    print("RN", rooms[0].RoomNumber)//  .RoomNumber)
-                    //print("title:", self.Room101Button.title(for: <#T##UIControl.State#>))
+                    //Turns all available rooms green
                     for room in rooms
                     {
                         print("Iteration:", room)
-                        //if room.RoomNumber == ;
-                    }
-                    if 201 == rooms[0].RoomNumber
-                    {
-                        print("Here")
-                        DispatchQueue.main.async {
-                            self.Room101Button.backgroundColor = UIColor.green
+                        if String(room.RoomNumber) == self.Room101Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room101Button.backgroundColor = UIColor.green
+                            }
                         }
-                        
+                        if String(room.RoomNumber) == self.Room102Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room102Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room103Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room103Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room104Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room104Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room105Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room105Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room106Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room106Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room107Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room107Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room108Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room108Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room109Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room109Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.Room110Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.Room110Button.backgroundColor = UIColor.green
+                            }
+                        }
+                        if String(room.RoomNumber) == self.RoomButton111Button.title(for: .normal)
+                        {
+                            DispatchQueue.main.async {
+                                self.RoomButton111Button.backgroundColor = UIColor.green
+                            }
+                        }
                     }
-                    */
+                    
                     //guard let Json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [String: Any] else { print(type(of: Json)); return }
                     //let Json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? Any
                     print("Before")
